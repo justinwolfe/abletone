5851feeb356db910fc720e6ae9249154fd5ec2f2
Justin Wolfe <justin@helpscout.com>
2023-10-06 23:14:59 -0700

    ts exploration

--- ableton.js
+++ ableton.js
@@ -8,18 +8,38 @@ const init = async () => {
   // Establishes a connection with Live
   await ableton.start();
 
-  ableton.song.startPlaying();
+  console.log(
+    await ableton.song.view.addListener('selected_track', (e) => {
+      console.log(e.raw.name);
+    })
+  );
 
-  // Observe the current playback state and tempo
-  ableton.song.addListener('is_playing', (p) => console.log('Playing:', p));
-  ableton.song.addListener('tempo', (t) => console.log('Tempo:', t));
+  ableton.song.addListener('scenes', (d) => console.log('Data:', d));
 
-  // Get the current tempo
-  const tempo = await ableton.song.get('tempo');
-  console.log('Current tempo:', tempo);
+  // ableton.on('message', (m) => console.log('Message:', m));
+  // const scenes = await ableton.song.get('scenes');
+  // const tracks = await ableton.song.get('tracks');
+  // console.log(await tracks[0].get('name'));
+  // // console.log(await tracks[0].get('name'));
 
-  // Set the tempo
-  await ableton.song.set('tempo', 85);
+  // // ableton.song.startPlaying();
+
+  // // // Observe the current playback state and tempo
+  // // ableton.song.addListener('is_playing', (p) => console.log('Playing:', p));
+  // // ableton.song.addListener('tempo', (t) => console.log('Tempo:', t));
+  // ableton.song.addListener('metronome', (m) => console.log('Metronome:', m));
+  // ableton.song.addListener('tracks', (t) => console.log('Tracks:', t));
+
+  // console.log(await ableton.song.get('tracks'));
+
+  // // Get the current tempo
+  // const tempo = await ableton.song.get('tempo');
+  // console.log('Current tempo:', tempo);
+
+  // // Set the tempo
+  // await ableton.song.set('tempo', 85);
 };
+
+init();
 
 export default init;
