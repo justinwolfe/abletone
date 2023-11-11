f2901e0424ea7659a274086114e5c6004afca444
Justin Wolfe <justin@helpscout.com>
2023-11-11 15:25:42 -0800

    use mix module

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import {
   selectMonitorTrack,
   TRACK_TYPES,
 } from './sceneTrackLogic.js';
+import { saveMix, restoreMix } from './mix.js';
 
 initMidi();
 let mixprint = {};
@@ -92,58 +93,13 @@ const init = async () => {
 
           break;
 
-        case '0': {
-          for (var track of state.tracks) {
-            const trackId = track.raw.id;
-            const mixerDevice = await track.get('mixer_device');
-            const volume = await mixerDevice.get('volume');
-            const panning = await mixerDevice.get('panning');
-            const sends = await mixerDevice.get('sends');
-            const activator = await mixerDevice.get('track_activator');
-            mixprint[trackId] = {
-              trackId: trackId,
-              trackName: track.raw.name,
-              activator: activator.raw.value,
-              volume: volume.raw.value,
-              panning: panning.raw.value,
-              sends: sends.map((send) => send.raw),
-            };
-          }
-          console.log('saved mix');
+        case '0':
+          await saveMix(ableton);
           break;
-        }
 
-        case '1': {
-          const tracks = await ableton.song.get('tracks', key);
-
-          for (key in mixprint) {
-            const track = tracks.find((track) => track.raw.id === key);
-
-            if (!track) {
-              return;
-            }
-
-            const mixerDevice = await track.get('mixer_device');
-            const activator = await mixerDevice.get('track_activator');
-            await activator.set('value', mixprint[key].activator);
-            const volume = await mixerDevice.get('volume');
-            await volume.set('value', mixprint[key].volume);
-            const panning = await mixerDevice.get('panning');
-            await panning.set('value', mixprint[key].panning);
-            const sends = await mixerDevice.get('sends');
-            for (var send of sends) {
-              const sendToPrint = mixprint[key].sends.find(
-                (capturedSend) => capturedSend.id === send.raw.id
-              );
-
-              if (sendToPrint) {
-                await send.set('value', sendToPrint.value);
-              }
-            }
-          }
-          console.log('restored mix');
+        case '1':
+          await restoreMix(ableton);
           break;
-        }
 
         case '2': {
           await selectMonitorTrack({ state, ableton, groupName: 'keys1' });
