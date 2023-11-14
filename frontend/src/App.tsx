c5e06a5d43ec51a7ab607909ad8479cd9da9aa6c
Justin Wolfe <justin@helpscout.com>
2023-11-13 20:48:00 -0800

    add readystate monitoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import useWebSocket from 'react-use-websocket';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 function App() {
   const { sendMessage, lastMessage, readyState } = useWebSocket(
     'ws://localhost:3000/ws'
   );
   const [apiState, setApiState] = useState({});
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
 
   useEffect(() => {
     if (!lastMessage?.data) {
@@ -19,6 +26,7 @@ function App() {
 
   return (
     <>
+      <div>{connectionStatus}</div>
       <div>{JSON.stringify(apiState)}</div>
     </>
   );
