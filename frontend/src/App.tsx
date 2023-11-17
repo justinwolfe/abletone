497bba0d141b8301e2adfa7fe2f03fa56e61960f
Justin Wolfe <justin@helpscout.com>
2023-11-16 21:18:15 -0800

    add some layout and color states

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import useWebSocket, { ReadyState } from 'react-use-websocket';
+import Paper from '@mui/material/Paper';
+import styled from 'styled-components';
+import classnames from 'classnames';
+import classNames from 'classnames';
 
 const getRecordingStatus = (number: number) => {
   if (number === 2) {
@@ -14,17 +18,61 @@ const getRecordingStatus = (number: number) => {
   return '';
 };
 
+const CenterCardUI = styled(Paper)`
+  width: 500px;
+  height: 500px;
+  padding: 30px;
+`;
+
+const TrackCardUI = styled(Paper)`
+  font-size: 30px;
+  text-align: center;
+  padding-left: 30px;
+  padding-right: 27px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin-bottom: 30px;
+  border-radius: 5px;
+`;
+
+const BackdropUI = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #f7f7ff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  transition: background-color 0.4s ease;
+
+  &.count-in {
+    background-color: #c7c7ff;
+  }
+
+  &.recording {
+    background-color: #8f8fff;
+  }
+`;
+
+const ConnectedUI = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  margin: 2px;
+  font-size: 12px;
+`;
+
 function App() {
   const { sendMessage, lastMessage, readyState } = useWebSocket(
     'ws://localhost:3000/ws'
   );
   const [apiState, setApiState] = useState({});
   const connectionStatus = {
-    [ReadyState.CONNECTING]: 'Connecting',
-    [ReadyState.OPEN]: 'Open',
-    [ReadyState.CLOSING]: 'Closing',
-    [ReadyState.CLOSED]: 'Closed',
-    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+    [ReadyState.CONNECTING]: 'connecting',
+    [ReadyState.OPEN]: 'open',
+    [ReadyState.CLOSING]: 'closing',
+    [ReadyState.CLOSED]: 'closed',
+    [ReadyState.UNINSTANTIATED]: 'uninstantiated',
   }[readyState];
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -55,14 +103,26 @@ function App() {
   } = apiState;
 
   return (
-    <>
-      <div>connection status: {connectionStatus}</div>
-      <div>TRACK: {selectedTrackName}</div>
-      <div>SCENE: {selectedSceneIndex}</div>
-      <div>recording: {getRecordingStatus(isRecording)}</div>
-      <div>playing: {isPlaying}</div>
-      <div>songTime: {songTime}</div>
-    </>
+    <BackdropUI
+      className={classNames(
+        isRecording === 2 && 'count-in',
+        isRecording === 1 && 'recording'
+      )}
+    >
+      <TrackCardUI>
+        <div>{selectedTrackName}</div>
+      </TrackCardUI>
+      <CenterCardUI>
+        <ConnectedUI>
+          <div>connection status: {connectionStatus}</div>
+        </ConnectedUI>
+
+        <div>SCENE: {selectedSceneIndex}</div>
+        <div>recording: {getRecordingStatus(isRecording)}</div>
+        <div>playing: {isPlaying}</div>
+        <div>songTime: {songTime}</div>
+      </CenterCardUI>
+    </BackdropUI>
   );
 }
 
